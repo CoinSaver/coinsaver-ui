59fe5fa99751030779ba576ba8226802fc3df2d9
Mike <Mdubbpro@gmail.com>
2017-12-06 11:52:39 -0800

    Re-merged down with Rich's changes

--- client/components/banks.js
+++ client/components/banks.js
@@ -1,12 +1,16 @@
 angular.module('coinsaver')
-  .component('banks', {
-    bindings: {
-    },
-    controller() {
-    },
-    template: `
-    <div>
-      This line is written in the banks.js component
-    </div>
-  `,
-  });
+.component('banks', {
+  bindings: {
+  },
+  controller() {
+    // const ctrl = this;
+
+    // this.plaidLink =
+  },
+  template: `
+  <div>
+    This line is written in the banks.js component c:
+    <button id="link-btn" >Link Account</button>
+  </div>
+`,
+});
