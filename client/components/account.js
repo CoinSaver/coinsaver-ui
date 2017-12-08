9b081de2210d9dafc5dc58f94ecedec96a857da3
Mike <Mdubbpro@gmail.com>
2017-12-07 19:12:59 -0800

    Added Firebase user settings object

--- client/components/account.js
+++ client/components/account.js
@@ -1,12 +1,23 @@
 angular.module('coinsaver')
-  .component('account', {
-    bindings: {
-    },
-    controller() {
-    },
-    template: `
-    <div>
-      This line is written in the account.js component
-    </div>
-  `,
-  });
+.component('account', {
+  bindings: {
+  },
+  controller() {
+    this.getCoinapi = function() {
+      console.log('ding')
+    };
+    
+  },
+  template: `
+  <div>
+    This line is written in the account.js component;
+
+    This button does everything:
+
+    <md-button md-autofocus class="md-primary" flex ng-click="$ctrl.getCoinapi()">
+      The Work Button
+    </md-button>
+
+  </div>
+`,
+});
