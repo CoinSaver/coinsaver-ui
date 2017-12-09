2d6c160332714903a40c853b3d424570d5e93388
balex8888 <balex8888@gmail.com>
2017-12-09 15:08:48 -0800

    Setting up config and private server settings

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const parser = require('body-parser');
 const path = require('path');
 const router = require('./router.js');
 // const db = require('../database/db.js'); //When we add the extremely complex coinsaver db
+const PRIVATE_IP = require('../config/config.js').AWS.PRIVATE_IP || null
 
 const app = express();
 
@@ -13,11 +14,12 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../client')));
 
-
 app.use(router);
 
-app.listen(port, () => {
-  // console.log('Server is running on port: ' + port)
-  console.log(`What does the server say about its port level? ITS OVER ${(port - 1).toString()}!! Port: ${port}`);
+app.listen(port, PRIVATE_IP, () => {
+  // console.log(`AWS Server is running on Port: ${port}`);
+  console.log(`What does the server say about its port number? ITS OVER ${(port - 1).toString()}!! Port: ${port}`);
 });
+
+
 
