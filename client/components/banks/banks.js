2150115a2404c42e6cda7627313a4d917cd0851c
Richard Oh <sioh89@gmail.com>
2017-12-12 14:00:24 -0800

    Choosing an account after bank login credentials working. Todo: uncomment filter for checking accounts only in plaidController and allow delinking

--- client/components/banks/banks.js
+++ client/components/banks/banks.js
@@ -6,7 +6,6 @@ angular.module('coinsaver')
     template: `
     <div>
       <md-button id="link-btn" class="md-raised md-primary" ng-if="$ctrl.linked===false" ng-click="$ctrl.checkClick()">Link A Bank Account</md-button>
-      <md-button id="test-btn" class="md-raised md-primary" ng-click="$ctrl.handleTest()">Test button</md-button>
       <transactions ng-repeat="item in $ctrl.transactions" info="item"/>
     </div>
     `,
@@ -30,36 +29,12 @@ angular.module('coinsaver')
           .then((res) => {
             console.log('Successful post to exchange tokens!', res);
 
-            // $http.get('/accounts')
-            //   .then((res) => {
-            //     console.log('Account data:');
-            //     console.log(res.data.accounts);
-            //     ctrl.accounts = res.data.accounts;
-            //   });
-
             $http.get('/transactions')
               .then((res) => {
                 console.log('User data:');
                 console.log(res.data);
-                ctrl.transactions = res.data;
-                ctrl.linked = true;
-
-                // Opens account selection overlay
-                // $mdDialog.show({
-                //   controller: DialogController,
-                //   templateUrl: '/components/banks/banks.html',
-                //   parent: angular.element(document.body),
-                //   targetEvent: null,
-                //   clickOutsideToClose: true,
-                //   fullscreen: ctrl.customFullscreen // Only for -xs, -sm breakpoints.
-                // })
-                //   .then(function(answer) {
-                //     ctrl.status = 'You said the information was "' + answer + '".';
-                //   }, function() {
-                //     ctrl.status = 'You cancelled the dialog.';
-                //   });
-                ctrl.handleTest();
-
+                ctrl.accounts = res.data;
+                ctrl.handleChooseAccount();
               });
           });
       },
@@ -88,51 +63,67 @@ angular.module('coinsaver')
     };
 
     this.status = '  ';
-    this.handleTest = (ev) => {
+    this.handleChooseAccount = (ev) => {
       $mdDialog.show({
         controller: DialogController,
         templateUrl: '/components/banks/banks.html',
         parent: angular.element(document.body),
         targetEvent: null,
-        clickOutsideToClose: true,
+        clickOutsideToClose: false,
+        escapeToClose: false,
         locals: {
-          transactions: ctrl.transactions,
+          accounts: ctrl.accounts,
         },
-        fullscreen: ctrl.customFullscreen // Only for -xs, -sm breakpoints.
+        fullscreen: ctrl.customFullscreen, // Only for -xs, -sm breakpoints.
       })
-      .then(function(answer) {
-        ctrl.status = 'You said the information was "' + answer + '".';
-        console.log(ctrl.status);
-      }, function() {
-        ctrl.status = 'You cancelled the dialog.';
-      });
+        .then((answer) => {
+          ctrl.transactions.push(ctrl.accounts[parseInt(answer)]);
+          ctrl.linked = true;
+        }, () => {
+          ctrl.linked = false;
+          ctrl.accounts = [];
+          ctrl.transactions = [];
+        });
     };
 
 
-    function DialogController($scope, $mdDialog, transactions) {
-      
-      $scope.transactions = transactions;
-      
-      $scope.hide = function() {
+    function DialogController($scope, accounts) {
+      $scope.accounts = accounts;
+
+      $scope.currentId = '';
+
+      $scope.hide = () => {
         $mdDialog.hide();
       };
-      
-      $scope.cancel = function() {
+
+      $scope.cancel = () => {
         $mdDialog.cancel();
       };
-      
-      $scope.answer = function(answer) {
+
+      $scope.answer = (answer) => {
         $mdDialog.hide(answer);
       };
 
+      $scope.selectedIndex;
+
+      $scope.setSelection = (index, name) => {
+        $scope.selectedIndex = index;
+        $scope.selectedItem = name;
+      };
+
+      $scope.selectedItem = 'Select an account';
+
+      $scope.getSelectedText = () => {
+        return $scope.selectedItem;
+      };
     }
 
+    // *Leave commented out for testing purposes. Links account
     $http.get('/transactions')
       .then((res) => {
         console.log('Transaction data:');
         console.log(res.data);
-        ctrl.transactions = res.data;
-        ctrl.linked = true;
+        ctrl.accounts = res.data;
+        ctrl.handleChooseAccount();
       });
-
   }).$inject = ['plaid'];
--- server/controllers/plaidController.js
+++ server/controllers/plaidController.js
@@ -61,7 +61,14 @@ module.exports = {
           console.log(err);
           res.send(err);
         }
-        res.json(JSON.parse(data));
+        const transactionsArray = JSON.parse(data);
+        const resObj = [];
+        for (let i = 0; i < transactionsArray.length; i++) {
+          // if (transactionsArray[i].type === 'checking') {
+            resObj.push(transactionsArray[i]);
+          // }
+        }
+        res.json(resObj);
       });
 
       // const startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
@@ -78,45 +85,51 @@ module.exports = {
       //   const resObj = [];
       //   const indexTrack = {};
 
-      //   for (let i = 0; i < transactionsResponse.accounts.length; i++) {
-      //     const accObj = {
-      //       id: transactionsResponse.accounts[i].account_id,
-      //       name: transactionsResponse.accounts[i].name,
-      //       type: transactionsResponse.accounts[i].subtype,
-      //       transactions: [],
-      //       roundSum: 0,
-      //       totalSum: 0,
-      //     };
-      //     // resObj.accounts.push(accObj);
-      //     // resObj.accounts.transactions = [];
-      //     resObj.push(accObj);
+        // for (let i = 0; i < transactionsResponse.accounts.length; i++) {
+        //   if (transactionsResponse.accounts[i].subtype === 'checking') {
+        //     const accObj = {
+        //       id: transactionsResponse.accounts[i].account_id,
+        //       name: transactionsResponse.accounts[i].name,
+        //       type: transactionsResponse.accounts[i].subtype,
+        //       transactions: [],
+        //       roundSum: 0,
+        //       totalSum: 0,
+        //     };
+        //     // resObj.accounts.push(accObj);
+        //     // resObj.accounts.transactions = [];
+        //     resObj.push(accObj);
 
-      //     // indexTrack just keeps track of which index in resObj array
-      //     // stores which account_id's information for quicker lookup
-      //     indexTrack[accObj.id] = resObj.length - 1;
-      //   }
+        //     // indexTrack just keeps track of which index in resObj array
+        //     // stores which account_id's information for quicker lookup
+        //     indexTrack[accObj.id] = resObj.length - 1;
+        //   }
+        // }
       //   // console.log('------', resObj);
       //   console.log('-------TRANSACTIONS', transactionsResponse.transactions[0]);
 
       //   for (let i = 0; i < transactionsResponse.transactions.length; i++) {
-      //     const temp = transactionsResponse.transactions[i];
+          // const temp = transactionsResponse.transactions[i];
 
-      //     const transactionObj = {
-      //       amount: temp.amount,
-      //       round: Math.ceil(temp.amount) - temp.amount,
-      //       date: temp.date,
-      //       location: {
-      //         city: temp.location.city,
-      //       },
-      //       name: temp.name,
-      //     };
-      //     // resObj.transactions[temp.account_id].push(transactionObj);
-      //     resObj[indexTrack[temp.account_id]].transactions.push(transactionObj);
-      //     resObj[indexTrack[temp.account_id]].totalSum += transactionObj.amount;
-      //     if (transactionObj.amount > 0) {
-      //       resObj[indexTrack[temp.account_id]].roundSum += transactionObj.round;
-      //     }
-      //   }
+          // If current transaction is for an account in our indexTrack
+          // it means this transactions is for a valid account
+          // if (indexTrack[temp.account_id] !== undefined){
+          //   const transactionObj = {
+          //     amount: temp.amount,
+          //     round: Math.ceil(temp.amount) - temp.amount,
+          //     date: temp.date,
+          //     location: {
+          //       city: temp.location.city,
+          //     },
+          //     name: temp.name,
+          //   };
+          //   // resObj.transactions[temp.account_id].push(transactionObj);
+          //   resObj[indexTrack[temp.account_id]].transactions.push(transactionObj);
+          //   resObj[indexTrack[temp.account_id]].totalSum += transactionObj.amount;
+          //   if (transactionObj.amount > 0) {
+          //     resObj[indexTrack[temp.account_id]].roundSum += transactionObj.round;
+          //   }
+          // }
+        // }
 
       //   console.log(`Pulled ${transactionsResponse.transactions.length} transactions`);
       //   console.log('-----', resObj);
