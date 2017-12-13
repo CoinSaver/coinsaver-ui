93ac139c0d871725eedb9714c47459b3d4f7804a
balex8888 <balex8888@gmail.com>
2017-12-12 20:49:12 -0800

    Email Controller created

---
+++ server/controllers/emailController.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+const coinsaverbot = require('../../config/config.js').coinsaverbot
+
+console.log('nodemailer: ', nodemailer);
+
+var transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // hostname
+    // secure: false, // use SSL
+    secureConnection: true,  
+    // port: 25, // port for secure SMTP
+    port: 465, // port for secure SMTP
+    auth: {
+      user: coinsaverbot.email2,
+      pass: coinsaverbot.pw2
+      // user: 'fake@gmail.com',
+      // pass: 'fake'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+var emailList = ['coinsaverbot@gmail.com', 'codeslingbot@gmail.com'];
+var ourMessage = 'This is the first email from CoinSaver, hope it goes through!';
+console.log('Button was clicked - server TEXT: ', ourMessage);
+var mailOptions = {
+  from: '"TestUser 🎂" <codeslingbot@gmail.com>',
+  to: emailList,
+  // subject: 'CodeslingBot Email Transcript using Node.js',
+  // text: `Users input code: ${'\n \n'} ${ourMessage}`
+  subject: 'Greetings from CodeslingBot',
+  text: `${ourMessage}`,
+  html: '<b>Html Test</b>'
+};
+transporter.sendMail(mailOptions, function(error, info) {
+  if (error) {
+    console.log('Your CodeslingEmail Error: ', error);
+  } else {
+    console.log('CodeSling Email sent: ' + info.response);
+  }
+});
+console.log('IN END of /send event');
--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const plaidController = require('./controllers/plaidController.js');
 const coinbaseController = require('./controllers/coinbaseController.js');
 const firebaseController = require('./controllers/firebaseController.js');
+const emailController = require('./emailController.js')
 
 router.post('/get_access_token', plaidController.get_access_token.post);
 router.get('/accounts', plaidController.accounts.get);
