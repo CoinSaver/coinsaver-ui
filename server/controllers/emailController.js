848eb6cd672ea9b6c7893921d1b3797fea2d9fb5
balex8888 <balex8888@gmail.com>
2017-12-12 22:10:18 -0800

    Send email subfunction created

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -10,8 +10,8 @@ var transporter = nodemailer.createTransport({
     // port: 25, // port for secure SMTP
     port: 465, // port for secure SMTP
     auth: {
-      user: coinsaverbot.email2,
-      pass: coinsaverbot.pw2
+      user: coinsaverbot.email,
+      pass: coinsaverbot.pw
       // user: 'fake@gmail.com',
       // pass: 'fake'
     },
@@ -22,23 +22,28 @@ var transporter = nodemailer.createTransport({
 
 // var emailList = ['coinsaverbot@gmail.com', 'codeslingbot@gmail.com'];
 var emailList = ['coinsaverbot@gmail.com'];
-var ourMessage = 'This is the first email from CoinSaver, hope it goes through!';
+var ourMessage = 'This is the 5 email from CoinSaver, hope it goes through!';
 console.log('Button was clicked - server TEXT: ', ourMessage);
 var mailOptions = {
-  from: '"TestUser 🎂" <codeslingbot@gmail.com>',
+  from: '"CoinSaverBot 🎂" <fakeemail@gmail.com>',
   to: emailList,
   // subject: 'CodeslingBot Email Transcript using Node.js',
   // text: `Users input code: ${'\n \n'} ${ourMessage}`
-  subject: 'Greetings from CodeslingBot',
+  subject: 'Greetings from CoinSaverBot',
   text: `${ourMessage}`,
-  // html: '<b>Html Test</b>'
+  html: `<b>Html Test</b> 
+    Our message is: ${ourMessage}`
 };
 
-// transporter.sendMail(mailOptions, function(error, info) {
-//   if (error) {
-//     console.log('Your CodeslingEmail Error: ', error);
-//   } else {
-//     console.log('CodeSling Email sent: ' + info.response);
-//   }
-// });
+var sendCoinsaverbotEmail = function (mailOptions) { 
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log('Your CodeslingEmail Error: ', error);
+    } else {
+      console.log('CodeSling Email sent: ' + info.response);
+    }
+  });
+  console.log('sendCoinsaverbotEmail function ran');
+}
+// sendCoinsaverbotEmail(mailOptions);
 console.log('IN END of /send event');
