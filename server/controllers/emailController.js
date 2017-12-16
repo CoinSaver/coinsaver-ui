5eae2bca31c4765db53bcf4c390ae67ba87a4982
balex8888 <balex8888@gmail.com>
2017-12-15 19:20:33 -0800

    Email controller reorganized, rebasing for auth updates

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -12,38 +12,40 @@ var transporter = nodemailer.createTransport({
     auth: {
       user: coinsaverbot.email,
       pass: coinsaverbot.pw
-      // user: 'fake@gmail.com',
-      // pass: 'fake'
+      // user: 'test@gmail.com',
+      // pass: 'test'
     },
     tls: {
         rejectUnauthorized: false
     }
 });
 
-// var emailList = ['coinsaverbot@gmail.com', 'codeslingbot@gmail.com'];
+
 var emailList = ['coinsaverbot@gmail.com'];
-var ourMessage = 'This is the 5 email from CoinSaver, hope it goes through!';
-console.log('Button was clicked - server TEXT: ', ourMessage);
+var emailMessage = 'This is the message from CoinSaverBot!';
+var emailSubject = 'Greetings from CoinSaverBot';
+var emailHtml = '<b>Html here</b>'
 var mailOptions = {
-  from: '"CoinSaverBot 🎂" <fakeemail@gmail.com>',
+  from: '"CoinSaverBot 🎂" <coinsaverbot@gmail.com>',
   to: emailList,
-  // subject: 'CodeslingBot Email Transcript using Node.js',
-  // text: `Users input code: ${'\n \n'} ${ourMessage}`
+  // subject: 'Coinsaverbot Email Transcript using Node.js',
+  // text: `Users input: ${'\n \n'} ${emailMessage}`
   subject: 'Greetings from CoinSaverBot',
-  text: `${ourMessage}`,
-  html: `<b>Html Test</b> 
-    Our message is: ${ourMessage}`
+  text: `${emailMessage}`,
+  html: `Our message is: ${emailMessage}
+  Our message is: ${emailHtml}`
 };
+console.log('Email was triggered - server TEXT: ', mailOptions);
 
 var sendCoinsaverbotEmail = function (mailOptions) { 
   transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
-      console.log('Your CodeslingEmail Error: ', error);
+      console.log('Your Coinsaverbot Email Error: ', error);
     } else {
-      console.log('CodeSling Email sent: ' + info.response);
+      console.log('Coinsaverbot Email sent: ' + info.response);
     }
   });
   console.log('sendCoinsaverbotEmail function ran');
 }
-// sendCoinsaverbotEmail(mailOptions);
-console.log('IN END of /send event');
+sendCoinsaverbotEmail(mailOptions);
+
