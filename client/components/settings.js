822741d8377a5751ebe01473fffc0e515ad967f2
Richard Oh <sioh89@gmail.com>
2017-12-16 16:40:33 -0800

    Finished account settings front end. Need to disable or make readonly if not editing

--- client/components/settings.js
+++ client/components/settings.js
@@ -11,141 +11,169 @@ angular.module('coinsaver')
     },
     template: `
       <h3>Account settings</h3>
-      <div ng-controller="AppCtrl" layout="column" ng-cloak style="max-width:50%;">
+      <div ng-controller="SettingsController" layout="column" ng-cloak style="max-width:600px;">
       
         <md-content layout-padding>
           <form name="settingsForm">
       
             <md-input-container class="md-block">
-              <md-switch class="md-primary" name="special" ng-model="autoPurchase" required>
+              <md-switch class="md-primary" name="special" ng-model="enableAutoPurchase" ng-disabled="true">
                 Enable auto-purchase
+                <md-tooltip class="tooltipcolor" md-direction="bottom">
+                  Please unlink your account from the banks tab
+                </md-tooltip>
               </md-switch>
             </md-input-container>
-      
-            <h4 style="margin-bottom:0px; padding-bottom:0px;">BTC:ETH purchase ratio</h4>
 
-            <div layout="row">
-              <md-input-container flex="50">
-                <md-slider-container>
+            <md-card>
+              <md-card-content>
 
-                  <md-icon class="mdicon" ng-disabled="bitcoin===0" ng-click="decBTC()">keyboard_arrow_left</md-icon>
+                <h4 style="margin-bottom:0px; padding-bottom:0px;">BTC:ETH purchase ratio</h4>
 
-                  <div layout="column" style="margin-right:15px; text-align:center;">
-                    <p style="margin-bottom:0px; padding-top:0px;"><b>BTC</b></p>
-                    <p style="margin-top:0px;">{{bitcoin}}</p>
+                <div layout="row">
+                  <md-input-container flex="50">
+                    <md-slider-container>
+
+                      <md-icon class="mdicon" ng-click="decBTC()">keyboard_arrow_left</md-icon>
+
+                      <div layout="column" style="margin-right:15px; text-align:center;">
+                        <p style="margin-bottom:0px; padding-top:0px;"><b>BTC</b></p>
+                        <p style="margin-top:0px;">{{bitcoin}}</p>
+                      </div>
+
+                      <md-slider min="0" max="100" ng-model="bitcoin" aria-label="ratioslider" ng-change="sliderControl()">
+                      </md-slider>
+
+                      <div layout="column" style="margin-left:15px; text-align:center;">
+                        <p style="margin-bottom:0px; padding-top:0px;"><b>ETH</b></p>
+                        <p style="margin-top:0px;">{{ethereum}}</p>
+                      </div>
+
+                      <md-icon class="mdicon" ng-click="decETH()">keyboard_arrow_right</md-icon>
+                      
+                    </md-slider-container>
+                  </md-input-container>
+                </div>
+
+                <div layout="row">
+                  <h4 style="margin-right:20px;">Auto-purchase frequency:</h4>
+
+                  <md-input-container>
+                    <md-select style="margin-top:-5px;" ng-model="frequency" aria-label="frequencyselect">
+                      <md-option ng-value="weekly" selected>weekly</md-option>
+                    </md-select>
+                  </md-input-container>
+                </div>
+
+                <div layout="column">
+                  <h4>Auto-purchase amount:</h4>
+
+                  <div class="autopurchase">
+
+                    <div layout="row">
+                      <md-input-container class="amountinput">
+                        <label>Minimum purchase</label>
+                        <input ng-model="minPurchase" type="tel" ng-blur="formatCurrency()">
+                      </md-input-container>
+
+                      <md-input-container class="amountinput">
+                        <label>Add'l Purchase</label>
+                        <input ng-model="autoPurchase" type="tel" ng-blur="formatCurrency()">
+                          <md-tooltip class="tooltipcolor" md-direction="top" style="margin-top:-20px;">
+                            (Optional) Set a flat, additional purchase amount
+                          </md-tooltip>
+                        </input>
+                      </md-input-container>
+
+                      <div>
+                        <md-input-container class="amountinput">
+                          <label>Total minimum</label>
+                          <input name="total" ng-model="totalMin" ng-min="5" min="5" ng-readonly="true" type="tel" required>
+                        </md-input-container>
+                        <p ng-show="totalMin < 5" style="color:red; margin-top:-40px; font-size:10px; margin-left:3px;">Total minimum must be at least $5</p>
+                      </div>
+                    </div>
+
+                    <div class="enablecheck">
+                      <input type="checkbox" ng-model="enableMaxPurchase">
+                        Set maximum purchase limit?
+                      </input>
+
+                      <div layout="row">
+                        <md-input-container class="amountinput" ng-style="{'visibility': enableMaxPurchase?'visible':'hidden'}">
+                          <label>Maximum purchase</label>
+                          <input ng-model="maxPurchase" type="tel" ng-blur="formatCurrency()">
+                        </md-input-container>
+                      </div>
+                    </div>
+
                   </div>
+                </div>
 
-                  <md-slider min="0" max="100" ng-model="bitcoin" aria-label="ratioslider" ng-change="sliderControl()">
-                  </md-slider>
+              </md-card-content>
 
-                  <div layout="column" style="margin-left:15px; text-align:center;">
-                    <p style="margin-bottom:0px; padding-top:0px;"><b>ETH</b></p>
-                    <p style="margin-top:0px;">{{ethereum}}</p>
-                  </div>
+              <md-card-actions layout="row" layout-align="end center">
+                <md-button ng-if="!enableEdit" ng-click="activateEdit()">Edit</md-button>
+                <md-button ng-if="enableEdit" ng-click="deactivateEdit()">Cancel</md-button>
+                <md-button ng-if="enableEdit">Save</md-button>
+              </md-card-actions>
+            </md-card>
 
-                  <md-icon class="mdicon" ng-disabled="ethereum===100" ng-click="decETH()">keyboard_arrow_right</md-icon>
-                  
-                </md-slider-container>
-              </md-input-container>
-      
-            </div>
-      
-            <md-input-container class="md-block">
-              <label>Client Email</label>
-              <input required type="email" name="clientEmail" ng-model="project.clientEmail"
-                     minlength="10" maxlength="100" ng-pattern="/^.+@.+\..+$/" />
-      
-              <div ng-messages="settingsForm.clientEmail.$error" role="alert">
-                <div ng-message-exp="['required', 'minlength', 'maxlength', 'pattern']">
-                  Your email must be between 10 and 100 characters long and look like an e-mail address.
-                </div>
-              </div>
-            </md-input-container>
-      
-            <md-input-container class="md-block">
-              <label>Hourly Rate (USD)</label>
-              <input required type="number" step="any" name="rate" ng-model="project.rate" min="800"
-                     max="4999" ng-pattern="/^1234$/" />
-      
-              <div ng-messages="settingsForm.rate.$error" multiple md-auto-hide="false">
-                <div ng-message="required">
-                  You've got to charge something! You can't just <b>give away</b> a Missile Defense
-                  System.
-                </div>
-      
-                <div ng-message="min">
-                  You should charge at least $800 an hour. This job is a big deal... if you mess up,
-                  everyone dies!
-                </div>
-      
-                <div ng-message="pattern">
-                  You should charge exactly $1,234.
-                </div>
-      
-                <div ng-message="max">
-                  {{settingsForm.rate.$viewValue | currency:"$":0}} an hour? That's a little ridiculous. I
-                  doubt even Bill Clinton could afford that.
-                </div>
-              </div>
-            </md-input-container>
-      
-            <md-input-container class="md-block">
-              <md-checkbox name="tos" ng-model="project.tos" required>
-                I accept the terms of service.
-              </md-checkbox>
-              <div ng-messages="settingsForm.tos.$error" multiple md-auto-hide="false">
-                <div ng-message="required">
-                  You must accept the terms of service before you can proceed.
-                </div>
-              </div>
-            </md-input-container>
-      
-            <md-input-container class="md-block">
-              <md-switch class="md-primary" name="special" ng-model="project.special" required>
-                Enable special options.
-              </md-switch>
-              <div ng-messages="settingsForm.special.$error" multiple>
-                <div ng-message="required">
-                  You must enable all special options before you can proceed.
-                </div>
-              </div>
-            </md-input-container>
-            <div>
-              <md-button type="submit">Submit</md-button>
-            </div>
-      
-            <p style="font-size:.8em; width: 100%; text-align: center;">
-              Make sure to include <a href="https://docs.angularjs.org/api/ngMessages" target="_blank">ngMessages</a> module when using ng-message markup.
-            </p>
           </form>
         </md-content>
       
       </div>
     `,
   })
-  .controller('AppCtrl', function($scope) {
+  .controller('SettingsController', ($scope) => {
     $scope.project = {
       description: 'Nuclear Missile Defense System',
       rate: 500,
-      special: true
+      special: true,
     };
 
-    $scope.autoPurchase = true;
+    $scope.enableAutoPurchase = true;
+    $scope.enableEdit = false;
     $scope.bitcoin = 50;
-    $scope.ethereum = 100-$scope.bitcoin;
+    $scope.ethereum = 100 - $scope.bitcoin;
+    $scope.frequency = 'weekly';
+    $scope.autoPurchase = 0.00;
+    $scope.minPurchase = 0.00;
+    $scope.totalMin = $scope.autoPurchase + $scope.minPurchase;
+    $scope.maxPurchase = 0.00;
+    $scope.noMaxPurchase = true;
 
     $scope.decBTC = () => {
-      $scope.bitcoin--;
-      $scope.ethereum++;
-    }
+      if ($scope.bitcoin !== 0) {
+        $scope.bitcoin--;
+        $scope.ethereum++;
+      }
+    };
 
     $scope.decETH = () => {
-      $scope.ethereum--;
-      $scope.bitcoin++;
-    }
+      if ($scope.ethereum !== 0) {
+        $scope.ethereum--;
+        $scope.bitcoin++;
+      }
+    };
 
     $scope.sliderControl = () => {
       $scope.ethereum = 100 - $scope.bitcoin;
+    };
+
+    $scope.formatCurrency = () => {
+      $scope.autoPurchase = parseFloat(parseFloat($scope.autoPurchase).toFixed(2)) || 0;
+      $scope.minPurchase = parseFloat(parseFloat($scope.minPurchase).toFixed(2)) || 0;
+      $scope.maxPurchase = parseFloat(parseFloat($scope.maxPurchase).toFixed(2)) || null;
+      $scope.totalMin = $scope.autoPurchase + $scope.minPurchase;
+    };
+
+    $scope.activateEdit = () => {
+      $scope.enableEdit = true;
+    };
+
+    $scope.deactivateEdit = () => {
+      $scope.enableEdit = false;
     };
 
   });
